89ed5639fdad94362dc6a659caf80187005155e1
Stephen Chiang <stephen.chiang@webstep.no>
2022-09-01 13:39:36 +0200

    [Lib] - fix: extra spacing in Position type default

--- src/lib/__types/types.ts
+++ src/lib/__types/types.ts
@@ -1,50 +1,44 @@
-export type Mode = "stacked" | "linear";
+export type Mode = 'stacked' | 'linear';
 
-export type Rounding = "nearest" | "up" | "down";
+export type Rounding = 'nearest' | 'up' | 'down';
 
-export type Position =
-  | "none"
-  | "top"
-  | "bottom"
-  | "left"
-  | "right"
-  | "default ";
+export type Position = 'none' | 'top' | 'bottom' | 'left' | 'right' | 'default';
 
-export type SortProperty = "none" | "largest" | "smallest";
+export type SortProperty = 'none' | 'largest' | 'smallest';
 
 export interface BarData {
-  color?: string;
-  background?: string;
-  label: string;
-  value: number;
-  percentage?: number;
-  e?: React.MouseEvent<HTMLDivElement, MouseEvent>;
-  k?: React.KeyboardEvent<HTMLDivElement>;
+    color?: string;
+    background?: string;
+    label: string;
+    value: number;
+    percentage?: number;
+    e?: React.MouseEvent<HTMLDivElement, MouseEvent>;
+    k?: React.KeyboardEvent<HTMLDivElement>;
 }
 
 export interface BarProps {
-  label: string;
-  color?: string;
-  background?: string;
-  mode?: Mode;
-  value: number;
-  portion?: number;
-  renderPortion?: number;
-  showPercentage?: boolean;
-  showTooltip?: boolean;
-  revealTooltipHandler?: (data: BarData | null) => void;
-  barClickHandler?: (d: Partial<BarData>) => Partial<BarData> | null;
+    label: string;
+    color?: string;
+    background?: string;
+    mode?: Mode;
+    value: number;
+    portion?: number;
+    renderPortion?: number;
+    showPercentage?: boolean;
+    showTooltip?: boolean;
+    revealTooltipHandler?: (data: BarData | null) => void;
+    barClickHandler?: (d: Partial<BarData>) => Partial<BarData> | null;
 }
 
 export interface ChartProps {
-  data?: BarProps[];
-  mode?: Mode;
-  roundTo?: Rounding;
-  sortBy?: SortProperty;
-  colorBackground?: string;
-  titlePosition?: Position;
-  showTooltip?: boolean;
-  showPercentage?: boolean;
-  clickHandler?: (d: Partial<BarData>) => any | null;
-  children?: any;
+    data?: BarProps[];
+    mode?: Mode;
+    roundTo?: Rounding;
+    sortBy?: SortProperty;
+    colorBackground?: string;
+    titlePosition?: Position;
+    showTooltip?: boolean;
+    showPercentage?: boolean;
+    clickHandler?: (d: Partial<BarData>) => any | null;
+    children?: any;
 }
